16b054b008345aa7f17076c3e7bf33678c2dcb4a
Santhosh <sansans90@gmail.com>
2017-09-27 09:16:48 +0000

    alarmtimer : fix build

--- fs/timerfd.c
+++ fs/timerfd.c
@@ -130,7 +130,6 @@ static bool timerfd_canceled(struct timerfd_ctx *ctx)
 
 static void timerfd_setup_cancel(struct timerfd_ctx *ctx, int flags)
 {
-	spin_lock(&ctx->cancel_lock);
 	if ((ctx->clockid == CLOCK_REALTIME ||
 	     ctx->clockid == CLOCK_REALTIME_ALARM) &&
 	    (flags & TFD_TIMER_ABSTIME) && (flags & TFD_TIMER_CANCEL_ON_SET)) {
